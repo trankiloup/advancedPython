1725da163a020f7772d0eca82f77b71172b4bb6d
trankiloup <bob1marc17@gmail.com>
2024-07-01 17:29:40 +0200

    manage

---
+++ advance/articles/views.py
@@ -0,0 +1,89 @@
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.utils import timezone
+from django.views.decorators.http import require_POST
+from django.views.generic import ListView, RedirectView, CreateView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LogoutView
+from django.views.generic import ListView, DetailView
+from .forms import RegisterForm, ArticleForm
+from .models import Article, UserFavouriteArticle
+
+
+class ArticleListView(ListView):
+    model = Article
+    template_name = 'article_list.html'
+    context_object_name = 'articles'
+
+
+class HomeView(RedirectView):
+    url = '/articles/'
+
+
+class UserLoginView(LoginView):
+    template_name = 'login.html'
+    success_url = reverse_lazy('home')
+
+
+class WhatIPublishView(LoginRequiredMixin, ListView):
+    model = Article
+    template_name = 'article_list.html'
+    context_object_name = 'articles'
+
+    def get_queryset(self):
+        return Article.objects.filter(author=self.request.user)
+
+
+class ArticleDetailView(LoginRequiredMixin, DetailView):
+    model = Article
+    template_name = 'ArticleDetailView.html'
+    context_object_name = 'article'
+
+
+class CustomLogoutView(LogoutView):
+    next_page = 'home'
+
+
+class FavoriteArticlesView(LoginRequiredMixin, ListView):
+    model = Article
+    template_name = 'FavoriteArticlesView.html'
+    context_object_name = 'articles'
+
+    def get_queryset(self):
+        return Article.objects.filter(userfavouritearticle__user=self.request.user)
+
+
+class RegisterView(CreateView):
+    form_class = RegisterForm
+    success_url = reverse_lazy('login')
+    template_name = 'register.html'
+
+    def form_valid(self, form):
+        # This method is called when valid form data has been POSTed.
+        # It should return an HttpResponse.
+        self.object = form.save(commit=False)
+        self.object.last_login = timezone.now()
+        self.object.save()
+        return super().form_valid(form)
+
+
+class PublishView(LoginRequiredMixin, CreateView):
+    model = Article
+    form_class = ArticleForm
+    template_name = 'publish.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('article-list')
+
+
+@require_POST
+def add_to_favorites(request):
+    article_id = request.POST.get('article_id')
+    article = Article.objects.get(id=article_id)
+    UserFavouriteArticle.objects.create(user=request.user, article=article)
+    return redirect('favorites')
---
+++ my_script.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+python3 -m venv django_venv
+source ./django_venv/bin/activate
+pip install -r requirement.txt
